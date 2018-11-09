6ff070bab6cf0e1292c22c6d7e51700acc795c19
Gabriel Yudi <g.sanefugi@gmail.com>
2018-11-08 22:23:08 -0200

    Mapa...

--- Projeto_CGLAB_M2/src/Classe/Mapa.java
+++ Projeto_CGLAB_M2/src/Classe/Mapa.java
@@ -5,6 +5,7 @@
  */
 package Classe;
 
+import java.util.ArrayList;
 import javax.media.opengl.GL2;
 import javax.media.opengl.glu.GLU;
 
@@ -14,27 +15,35 @@ import javax.media.opengl.glu.GLU;
  */
 public class Mapa 
 {
-     GLU glu = new GLU();
-    
+    GLU glu = new GLU();
+     
     //Pede para gerar o mapa X
     public void gerarMapa(int id, GL2 gl)
     {
+        double[][] listaInfo = null;
+        
         switch(id)
         {
             case 1:
-                m1(gl);
+                listaInfo = m1();
             break;
               
             case 2:
-                m2(gl);
+               // listaInfo = m2();
             break;
             
             default: 
-                throw new UnsupportedOperationException("ID mapa incorreto");
+                throw new UnsupportedOperationException("ERRO - ID mapa incorreto");
         }
+        if(listaInfo == null)
+        {
+            throw new UnsupportedOperationException("ERRO - listaInfo retornou NULL");
+        }
+        
+        renderizar(listaInfo, gl);
     }
     
-    //Cria um piso neste ponto (0-> padrao, 1-> inicio, 2->fim)
+    //Cria um piso neste ponto (0-> inicio, 1-> fim, 2ou+->piso)
     private void gerarPiso(GL2 gl,int tipo)
     {
         gl.glPushMatrix();
@@ -51,20 +60,28 @@ public class Mapa
         gl.glPopMatrix();
     }
     
-    //Cada funcao é um mapa
-    private void m1(GL2 gl) 
+    //Retorna uma lista com os dados da coordenada X,Y,Z 
+    //(Foram separado numa funcao para diminui a complexidade)
+    //OBS.: primeiro => inicio e Segundo => fim
+    private double[][] m1() 
     {
-        gl.glPushMatrix();
-            
-        
-        gl.glPopMatrix();
+        double[][] temp = 
+        {{1,2,3},
+        {1,2,3}};
+                
+        return temp;
     }
-
-    private void m2(GL2 gl) 
+    
+    //OBS.: listaInfo: 
+    //o primeiro => inicio e 
+    //o segundo => fim
+    private void renderizar(double[][] listaInfo, GL2 gl) 
     {
-        gl.glPushMatrix();
-            
-        
-        gl.glPopMatrix();
+        for (int i = 0; i < listaInfo.length; i++) 
+        {
+            gl.glPushMatrix();
+            gl.glTranslated(listaInfo[i][0],listaInfo[i][1],listaInfo[i][2]]);
+            gl.glPopMatrix();
+        }
     }
 }
