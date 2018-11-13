df5b6725221d4a56d1bb62c74497587679ee4224
Dino <papaleguas95.19@gmail.com>
2018-11-12 22:31:00 -0200

    cubo criado

--- Projeto_CGLAB_M2/src/Classe/Cubo.java
+++ Projeto_CGLAB_M2/src/Classe/Cubo.java
@@ -5,10 +5,69 @@
  */
 package Classe;
 
+import javax.media.opengl.GL2;
+
 /**
  *
  * @author Yudi-PC
  */
 public class Cubo {
+    public void cubo(GL2 gl){
+        frente(gl);
+        direita(gl);
+        tras(gl);
+        esquerda(gl);
+        cima(gl);
+        baixo(gl);
+    }
+    
+    private void frente(GL2 gl){
+        gl.glBegin(GL2.GL_QUADS);
+            gl.glVertex3d(-0.5,-0.5,0.5);
+            gl.glVertex3d(0.5,-0.5,0.5);
+            gl.glVertex3d(0.5,0.5,0.5);
+            gl.glVertex3d(-0.5,0.5,0.5); 
+        gl.glEnd();
+    }
+    private void direita(GL2 gl){
+        gl.glBegin(GL2.GL_QUADS);
+            gl.glVertex3d(0.5,-0.5,0.5);
+            gl.glVertex3d(0.5,-0.5,-0.5);
+            gl.glVertex3d(0.5,0.5,-0.5);
+            gl.glVertex3d(0.5,0.5, -0.5); 
+        gl.glEnd();
+    }
+    private void tras(GL2 gl){
+        gl.glBegin(GL2.GL_QUADS);
+            gl.glVertex3d(-0.5,-0.5,-0.5);
+            gl.glVertex3d(0.5,-0.5,-0.5);
+            gl.glVertex3d(0.5,0.5,-0.5);
+            gl.glVertex3d(-0.5,0.5,-0.5);
+        gl.glEnd();
+    }
+    private void esquerda(GL2 gl){
+        gl.glBegin(GL2.GL_QUADS);
+            gl.glVertex3d(-0.5,-0.5,0.5);
+            gl.glVertex3d(-0.5,0.5,0.5);
+            gl.glVertex3d(-0.5,0.5, -0.5);
+            gl.glVertex3d(-0.5,-0.5,-0.5);
+        gl.glEnd();
+    }
+    private void cima(GL2 gl){
+        gl.glBegin(GL2.GL_QUADS);
+            gl.glVertex3d(-0.5,0.5,0.5);
+            gl.glVertex3d(0.5,0.5,0.5);
+            gl.glVertex3d(0.5,0.5,-0.5);
+            gl.glVertex3d(-0.5,0.5,-0.5);
+        gl.glEnd();
+    }
+    private void baixo(GL2 gl){
+        gl.glBegin(GL2.GL_QUADS);
+            gl.glVertex3d(-0.5, -0.5, 0.5);
+            gl.glVertex3d(0.5,-0.5,0.5);
+            gl.glVertex3d(0.5,-0.5,-0.5);
+            gl.glVertex3d(-0.5,-0.5,-0.5);
+        gl.glEnd();
+    }
     
 }
--- Projeto_CGLAB_M2/src/Jogo/Jogo.java
+++ Projeto_CGLAB_M2/src/Jogo/Jogo.java
@@ -2,10 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Jogo;
 
 import Classe.*;
+import com.jogamp.opengl.util.Animator;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.media.opengl.GL;
@@ -20,19 +20,18 @@ import javax.swing.JFrame;
  *
  * @author Glauco
  */
-public class Jogo 
-             implements GLEventListener {
-
-    GLU glu = new GLU();
-
+public class Jogo
+        implements GLEventListener {
     
-    public static void main(String args[])
-    {
+    GLU glu = new GLU();
+    
+    public static void main(String args[]) {
         new Jogo();
     }
+    private double r;
+    private boolean mostra=true;
     
-    public Jogo()
-    {
+    public Jogo() {
         GLJPanel canvas = new GLJPanel();
         canvas.addGLEventListener(this);
         
@@ -40,7 +39,7 @@ public class Jogo
         frame.setSize(700, 700); //define o tamanho da tela
         frame.getContentPane().add(canvas);
         frame.setVisible(true);
-      
+        
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -51,45 +50,54 @@ public class Jogo
                 }).start();
             }
         });
-
-    
+        
     }
-    
-    
     
     public void init(GLAutoDrawable glAuto) {
-        GL gl = glAuto.getGL(); 
+          GL2 gl = glAuto.getGL().getGL2();     
         gl.glClearColor(0.4f, 0.4f, 0.4f, 0.4f); //define a cor de fundo
         gl.glEnable(GL.GL_DEPTH_TEST); //teste de profundidade
+        
+      
+
+        Animator ani = new Animator(glAuto);
+        ani.start();
     }
+    Cubo c1 = new Cubo();
 
     public void display(GLAutoDrawable glAuto) {
-
+        
         GL2 gl = glAuto.getGL().getGL2();
         
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         
         gl.glLoadIdentity(); //Renicia todos acumulativos
-        gl.glTranslated(0,0,-20); //Onde estara a camera em posição
+        gl.glTranslated(0, 0, -20); //Onde estara a camera em posição
         //Descobrir como deixa a camera de lado para gera um efeito 3D
         
+        gl.glRotated(r, 1, 1, 1);
+        r = r + 0.05;
+
         //Chamando o mapa
-        new Mapa().gerarMapa(1, gl);
+        //new Mapa().gerarMapa(1, gl);
         
         
+        if(mostra)
+        c1.cubo(gl);
         
-    }
-    public void reshape(GLAutoDrawable gLAutoDrawable, int x, int y, int w, int h) {
-  
-        GL2 gl = gLAutoDrawable.getGL().getGL2(); 
-        gl.glMatrixMode(GL2.GL_PROJECTION);
-        gl.glLoadIdentity();
-        glu.gluPerspective(60,1,1,30);
-        gl.glMatrixMode(GL2.GL_MODELVIEW);
-        gl.glLoadIdentity();
-        gl.glTranslated(0,0,-5);
     }
     
+    public void reshape(GLAutoDrawable gLAutoDrawable, int x, int y, int w, int h) {
+        
+        GL2 gl = gLAutoDrawable.getGL().getGL2();        
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+        glu.gluPerspective(60, 1, 1, 30);
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslated(0, 0, -5);
+    }
+
     /*
      public void reshape(GLAutoDrawable drawable, int x, int y, int w, int h) {
         GL2 gl = drawable.getGL().getGL2();
@@ -113,25 +121,12 @@ public class Jogo
         gl.glMatrixMode(GL2.GL_MODELVIEW);
         gl.glLoadIdentity();
     }*/
-
-
     public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {
-      
+        
     }
-
+    
     public void dispose(GLAutoDrawable glad) {
-
+        
     }
-
+    
 }
-
-
-
-
-
-
-
-
-
-
-
